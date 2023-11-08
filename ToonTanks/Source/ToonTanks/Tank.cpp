d7efc9adc6064b5b1b20bb394399333f9afa9f83
dtm-dplusplus <129973785+dtm-dplusplus@users.noreply.github.com>
2023-11-08 19:32:32 +0000

    Set up enhanced input mapping
    Set up enhanced input mapping context and input actions. No longer using depricated axis mappings.

--- ToonTanks/Source/ToonTanks/Tank.cpp
+++ ToonTanks/Source/ToonTanks/Tank.cpp
@@ -5,6 +5,10 @@
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
+#include "InputAction.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
 ATank::ATank()
 {
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>("Spring Arm");
@@ -12,3 +16,37 @@ ATank::ATank()
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	Camera->SetupAttachment(SpringArm);
 }
+
+void ATank::BeginPlay()
+{
+	ABasePawn::BeginPlay();
+
+}
+
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent); //Call the parent version
+
+	// Get the player controller
+	auto playerController = Cast<APlayerController>(GetController());
+
+	// Get the local player enhanced input subsystem
+	auto eiSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(playerController->GetLocalPlayer());
+	//Add the input mapping context
+	eiSubsystem->AddMappingContext(InputMapping, 0);
+
+	// Get the EnhancedInputComponent
+	auto playerEIcomponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	//Bind Move() to the mapping
+	//BindAction for enhanced system takes Action, ETriggerEvent, object, and function
+	//ETriggerEvent is an enum, where Triggered means "button is held down".
+	playerEIcomponent->BindAction(InputMoveForward, ETriggerEvent::Triggered, this, &ATank::Move);
+}
+
+void ATank::Move(const FInputActionValue& Value)
+{
+	UE_LOG(LogTemp, Display, TEXT("Float value: %f"), Value.Get<float>());
+}
+
--- ToonTanks/Source/ToonTanks/Tank.h
+++ ToonTanks/Source/ToonTanks/Tank.h
@@ -6,18 +6,41 @@
 #include "BasePawn.h"
 #include "Tank.generated.h"
 
+struct FInputActionValue;
+
 UCLASS()
-class TOONTANKS_API ATank : public ABasePawn
+class TOONTANKS_API ATank final : public ABasePawn
 {
 	GENERATED_BODY()
 
 public:
 	ATank();
 
+	void BeginPlay() override;
+	void Move(const FInputActionValue& Value);
 private:
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	class UCameraComponent* Camera;
 							 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	class USpringArmComponent* SpringArm;
+
+	//Setting up inputs. Mapping context and Input Action variables, to be set in Blueprint
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* InputMapping;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+	class UInputAction* InputMoveForward;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+	class UInputAction* InputTurn;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+	class UInputAction* InputRotateTurret;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input", meta = (AllowPrivateAccess = "true"))
+	class UInputAction* InputFire;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 };
